d76db9128169c01e39d0c6eff2515fef27b322dc
James Cross <jcross2774@gmail.com>
2017-10-21 22:10:29 -0600

    please work

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,10 @@ AdminUser.create(email: "jcross2774@gmail.com", password: "JrossC2774#!", passwo
 
 puts "1 AdminUser created"
 
+AdminUser.create(email: "jcross2774@gmail.com", password: "JrossC2774#!", password_confirmation: "JrossC2774#!", first_name: "James", last_name: "Cross")
+
+puts "1 AdminUser created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
