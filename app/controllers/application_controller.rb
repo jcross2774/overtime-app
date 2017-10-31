06f8bf88bfab91c95b6a123d997d926db4d3e683
James Cross <jcross2774@gmail.com>
2017-10-30 22:35:50 -0600

    Impemented redirect non authorised users - application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   
 
   private
