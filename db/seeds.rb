6452c842cb28bddd1ad5c558c914acc362c268e8
James Cross <jcross2774@gmail.com>
2017-11-04 16:28:39 -0600

    Integrated phone validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
                     password: "asdfasdf", 
                     password_confirmation: "asdfasdf",
                     first_name: "Jon", 
-                    last_name: "Snow"
+                    last_name: "Snow",
                     phone: "8018511604")
 
 puts "1 User created"
@@ -11,7 +11,7 @@ AdminUser.create(email: "admin@test.com",
                  password: "asdfasdf", 
                  password_confirmation: "asdfasdf", 
                  first_name: "Admin", 
-                 last_name: "Name"
+                 last_name: "Name",
                  phone: "8018511604")
 
 puts "1 Admin User created"
